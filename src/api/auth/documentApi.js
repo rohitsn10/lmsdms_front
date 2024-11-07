456fcd55ad1bf793d9fac15cc4f55e85ceb6c748
internsn10 <156306755+internsn10@users.noreply.github.com>
2024-11-07 12:36:20 +0530

    fatch data error

--- src/api/auth/documentApi.js
+++ src/api/auth/documentApi.js
@@ -25,7 +25,6 @@ export const documentApi = createApi({
       transformResponse: (response) => response.data,
     }),
 
-
     createDocumentType: builder.mutation({
       query: (documentTypeData) => ({
         url: "dms_module/create_get_document_type",
@@ -71,39 +70,6 @@ export const documentApi = createApi({
         method: "GET",
       }),
       transformResponse: (response) => response.data,
-
-    endpoints: (builder) => ({
-        createDocument: builder.mutation({
-            query: (documentData) => ({
-                url: 'dms_module/create_document',
-                method: 'POST',
-                body: documentData,
-            }),
-            transformResponse: (response) => response.data,
-        }),
-
-        createDocumentType: builder.mutation({
-            query: (documentTypeData) => ({
-                url: 'dms_module/create_get_document_type',
-                method: 'POST',
-                body: documentTypeData,
-            }),
-            transformResponse: (response) => response.data,
-        }),
-
-        fetchDocumentTypes: builder.query({
-            query: () => ({
-                url: 'dms_module/get_document_type',
-                method: 'GET',
-            }),
-            transformResponse: (response) => response.data,
-        }),
-
-        fetchDocuments: builder.query({
-            query: () => 'dms_module/view_document',
-            transformResponse: (response) => response.data,
-        }),
-
     }),
   }),
 });
